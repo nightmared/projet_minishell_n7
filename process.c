aee86b29e9e8654dc993a61973c5775904616237
Simon THOBY <simonthoby@live.fr>
2019-04-15 18:28:34 +0200

    yay ! les pipes marchent maitenant

--- process.c
+++ process.c
@@ -102,59 +102,69 @@ void scan_background_processes(struct list **bkg_proc) {
     }
 }
 
+int run_with_pipe(struct command_line *current) {
+    // traitement du pipeline
+    if (current->next_pipe != NULL) {
+        // Nouveau pipe pour la communication entre les deux processus
+        int pipefd[2];
+        // on ne fait pas de vérification et on croise les doigts pour que les ulimits soient assez élevées
+        pipe(pipefd);
+
+        pid_t pid = fork();
+        if (pid < -1) {
+            dprintf(STDERR_FILENO, "Impossible de forker, problème de mémoire ?\n");
+            return EXIT_FAILURE;
+        } else if (pid == 0) {
+            // on connecte la sortie du processus précédent avec l'entrée du nouveau processus
+            close(pipefd[1]);
+            dup2(pipefd[0], STDIN_FILENO);
+
+            return run_with_pipe(current->next_pipe);
+        } else {
+            close(pipefd[0]);
+            // on redirige notre sortie standard vers le nouveau processus
+            int new_output = dup2(pipefd[1], STDOUT_FILENO);
+        }
+    }
+
+    // On mets à jour les directions des flux de données
+    if (current->output_stream) {
+        if (freopen(current->output_stream, "w", stdout) == NULL) {
+            dprintf(STDERR_FILENO, "Échec de l'ouverture du fichier %s\n", current->output_stream);
+            return EXIT_FAILURE;
+        }
+    }
+    if (current->error_stream) {
+        if (freopen(current->error_stream, "w", stderr) == NULL) {
+            dprintf(STDERR_FILENO, "Échec de l'ouverture du fichier %s\n", current->error_stream);
+            return EXIT_FAILURE;
+        }
+    }
+    if (current->input_stream) {
+        if (freopen(current->input_stream, "r", stdin) == NULL) {
+            dprintf(STDERR_FILENO, "Échec de l'ouverture du fichier %s\n", current->input_stream);
+            return EXIT_FAILURE;
+        }
+    }
+
+
+    // On exécute la commande en cherchant dans le path
+    if (execvp(current->words[0], current->words) < 0) {
+        dprintf(STDERR_FILENO, "Commande '%s' non trouvée dans votre $PATH\n", current->words[0]);
+        return EXIT_FAILURE;
+    }
+
+    // présent uniquement pour faire plaisir au compilateur, puisqu'on n'arrivera jamais jusque ici
+    return EXIT_SUCCESS;
+}
+
 int run_command(struct command_line *cmd) {
-    // on masque les signaux sigtstp et sigint, malheureusement
+    // on masque les signaux sigtstp et sigint, malheureusement (cf. moodle)
     sigset_t set;
     sigemptyset(&set);
     sigaddset(&set, SIGTSTP);
     sigaddset(&set, SIGINT);
     sigprocmask(SIG_SETMASK, &set, NULL);
 
-    // On mets à jour les directions des flux de données
-    if (cmd->output_stream) {
-        if (freopen(cmd->output_stream, "w", stdout) == NULL) {
-            dprintf(STDERR_FILENO, "Échec de l'ouverture du fichier %s\n", cmd->output_stream);
-            return EXIT_FAILURE;
-        }
-    }
-    if (cmd->error_stream) {
-        if (freopen(cmd->error_stream, "w", stderr) == NULL) {
-            dprintf(STDERR_FILENO, "Échec de l'ouverture du fichier %s\n", cmd->error_stream);
-            return EXIT_FAILURE;
-        }
-    }
-    if (cmd->input_stream) {
-        if (freopen(cmd->input_stream, "r", stdin) == NULL) {
-            dprintf(STDERR_FILENO, "Échec de l'ouverture du fichier %s\n", cmd->input_stream);
-            return EXIT_FAILURE;
-        }
-    }
-
-    // traitement du pipeline
-    struct command_line *pipe = cmd->next_pipe;
-    while (pipe != NULL) {
-        pid_t pid = fork();
-        if (pid < -1) {
-            dprintf(STDERR_FILENO, "Impossible de forker, problème de mémoire ?\n");
-            exit(1);
-        } else if (pid == 0) {
-            // on connecte la sortie du processus précédent avec l'entrée du nouveau processus
-
-            // on exécute le processus fils
-            if (execvp(pipe->words[0], pipe->words) < 0) {
-                dprintf(STDERR_FILENO, "Commande '%s' non trouvée dans votre $PATH\n", pipe->words[0]);
-                return EXIT_FAILURE;
-            }
-        }
-        pipe = pipe->next_pipe;
-    }
-
-    // On exécute la commande en cherchant dans le path
-    if (execvp(cmd->words[0], cmd->words) < 0) {
-        dprintf(STDERR_FILENO, "Commande '%s' non trouvée dans votre $PATH\n", cmd->words[0]);
-        return EXIT_FAILURE;
-    }
-
-    // présent uniquement pour faire plaisir au compilateur, puisqu'on n'arrivera jamais jusque ici
-    return EXIT_SUCCESS;
+    run_with_pipe(cmd);
 }
